c5af9967202f7bd11d5f53481f9dbb359a9dd5b0
nickybayat <nickybayat95@gmail.com>
2020-05-30 18:58:36 -0400

    pretrained srgan github

--- super_resolve.py
+++ super_resolve.py
@@ -1,2 +1,4 @@
 # Each of the LR faces is upscaled to 224x224 using of the SR techniques.
 # (a) bicubic interpolation, (b) SCN, (c) sparse representation super-resolution (ScSR), (d) LapSRN, (e) SRGAN
+
+# https://github.com/krasserm/super-resolution
