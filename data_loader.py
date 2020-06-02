bf0a4f32be27c355075575abf8f68849d1bcba78
nicky bayat <nickybayat95@gmail.com>
2020-06-02 14:14:48 -0400

    trying to detect frontal faces in LFW

--- data_loader.py
+++ data_loader.py
@@ -7,3 +7,30 @@
 # face images #01 and #14 respectively of each subject. Since
 # the AR dataset has 100 subjects, we obtained 100 LR–HR
 # pairs of face images for this experiment.
+
+
+import dlib
+import imageio
+
+
+def faceDetect(imgArray):
+    detector = dlib.get_frontal_face_detector()
+    # Run the face detector, upsampling the image 1 time to find smaller faces.
+    dets = detector(imgArray, 1)
+    if len(dets) >= 1:
+        return True
+    else:
+        return False
+
+
+def main():
+    num = input("please enter number of the image: ")
+    while num != str(-1):
+        img_path = "Abdullah_Gul/Abdullah_Gul_{}.jpg".format(num)
+        img = imageio.imread(img_path, pilmode='RGB')
+        print(faceDetect(img))
+        num = input("please enter number of the image: ")
+
+
+if __name__ == "__main__":
+    main()
