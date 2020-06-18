48c5b55d9d7d82090c3aa7bc8f099cfb5117914d
nickybayat <nickybayat95@gmail.com>
2020-06-18 12:48:58 +0430

    rank 1 computed

--- LRFR.py
+++ LRFR.py
@@ -1,5 +1,61 @@
-# Get HR-LR pair from data_loader
+# Get HR-LR pair
 # Super resolve LR face
 # Extract feature vectors of both super-resolved face and original HR
 # Use cosine distance to match score between feature vectors
+import os
+import extract_embedding as ee
+from scipy import spatial
+import pickle
 
+
+def compute_cosine(gallery_embedding, probe_embedding):
+    return spatial.distance.cosine(gallery_embedding, probe_embedding)
+
+
+def main():
+    fake_path = "/imaging/nbayat/AR/LRFR_Pairs/fake_HR"
+    hr_path = "/imaging/nbayat/AR/LRFR_Pairs/HR"
+
+    hr_samples = pickle.load(open("gallery_embeddings.pickle","rb"))
+    lr_samples = pickle.load(open("probe_embeddings.pickle","rb"))
+    """
+    samples = {}
+    path = fake_path
+    for idx, filename in enumerate(os.listdir(path)):
+        if filename not in samples.keys():
+            print(idx+1, "Filename: ", filename)
+            face = os.path.join(path, filename)
+            embd = ee.predict(face)
+            samples[filename] = embd
+    pickle.dump(samples, open("probe_embeddings.pickle", "wb"))
+    print("----All probe embeddings are saved in hr_samples----")
+    """
+
+    print("length of hr pickle file: ", len(hr_samples.keys()))
+    print("length of lr pickle file: ", len(lr_samples.keys()))
+    rank = 1
+    score = 0
+    for filename in os.listdir(fake_path):
+        print("Probe Image: ", filename)
+        cosine_distances = []
+        hr_files = []
+        fake_embd = lr_samples[filename]
+        parts = filename.split('-')
+        parts[len(parts) - 1] = "14.jpg"
+        GT_filename = '-'.join(parts)
+        for hr_filename in os.listdir(hr_path):
+            hr_files.append(hr_filename)
+            hr_embd = hr_samples[hr_filename]
+            cosine_distances.append(compute_cosine(hr_embd, fake_embd))
+
+        index = cosine_distances.index(min(cosine_distances))
+        if hr_files[index] == GT_filename:
+            print("probe {} matched with {}".format(filename, hr_files[index]))
+            score += 1
+
+    print("Rank {} accuracy is: {}".format(rank, score/100))
+
+
+
+if __name__ == "__main__":
+    main()
--- extract_embedding.py
+++ extract_embedding.py
@@ -7,21 +7,25 @@ from keras.layers import Input
 from keras_vggface.vggface import VGGFace
 from keras_vggface import utils
 
-def predict(model, img_path, classify=True):
+
+def predict(img_path, classify=False):
+    model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg') # pooling: None, avg or max
     img = image.load_img(img_path, target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = utils.preprocess_input(x, version=2)
     preds = model.predict(x)
     if classify:
-        print('Predicted:', utils.decode_predictions(preds))
+        # print('Predicted:', utils.decode_predictions(preds))
+        return utils.decode_predictions(preds)
     else:
-        print("Features: ", preds.shape)
+        # print("Features: ", preds.shape)
+        return preds
 
 
 def extract_features(img_path):
     vgg_features = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg') # pooling: None, avg or max
-    predict(vgg_features, img_path, classify=False)
+    return predict(vgg_features, img_path, classify=False)
     # if you want to extract features of a specific layer
     # layer_name = 'layer_name' # edit this line
     # vgg_model = VGGFace() # pooling: None, avg or max
@@ -30,9 +34,8 @@ def extract_features(img_path):
 
 
 def main():
-    vggface = VGGFace(model='resnet50') # models: vgg16, resnet50, senet50
-    predict(vggface, "./SRGAN.png")
-    extract_features("./SRGAN.png")
+    # vggface = VGGFace(model='resnet50') # models: vgg16, resnet50, senet50
+    predict("./SRGAN.png", classify=False)
 
 
 if __name__ == "__main__":
